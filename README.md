28f44ea088d735974d2addf5cb83c62008c2baf7
Peter Bolier <bolier@xs4all.nl>
2020-09-22 15:08:27 +0000

    Added pollution and weather api calls. todo: exception handling and code clean up.

--- README.md
+++ README.md
@@ -6,3 +6,27 @@ For now it's a very basic application, just using flask.
 We did consider to use a local MQTT broker to which the raw measurement data would be published, enabling further processing, like calculating averages over 1 or 10 minues (which is a common interval for environmental data). 
 
 We also considere using FastAPI and documenting the API using Swagger, but installing additonal packages, ie. uvicorn failed (dependecy on gcc <todo: check details>) and it took more space than we liked anyway.  
+
+Testrun sensor service
+.. 
+prequisites
+- flask...
+cd were app is installed
+
+export FLASK_APP=sensor-rest-api.py
+run flask
+
+on some host
+
+curl http://127.0.0.1:5000/pollution
+2020-09-22 13:54:46.816 INFO     127.0.0.1 - - [22/Sep/2020 13:54:46] "GET /pollution HTTP/1.1" 200 -
+{ "instant":2020-09-22T13:54:46.775000, "PM1_0":7, "PM2_5":11, "PM10":19, "PM1_0_atm":7, “PM2_5_atm":11, "PM10_atm":19, "gt0_3um":0, "gt0_5um":0, "gt1_0um":0, "gt2_5um":0, "gt5_0um":0, "gt10um":0}
+
+curl http://127.0.0.1:5000/gas
+2020-09-22 13:55:43.833 INFO     127.0.0.1 - - [22/Sep/2020 13:55:43] "GET /gas HTTP/1.1" 200 -
+{ "instant":2020-09-22T13:55:43.715350, "adc":0.636, "nh3":121011.49425287354, "oxidised":50574.39446366783, "reduced":1128615.3846153868}
+
+
+curl http://127.0.0.1:5000/weather
+2020-09-22 13:57:33.690 INFO     127.0.0.1 - - [22/Sep/2020 13:57:33] "GET /weather HTTP/1.1" 200 -
+{ "instant":2020-09-22T13:57:33.529210, "temperature":24.18012930811965, "pressure":652.033925549806, "humidity":89.0146080309965}
--- sensor-rest-api.py
+++ sensor-rest-api.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python3
 
-import ST7735
-from datetime import datetime
+"""
+Simple application creating a REST-API to read Pimoroni enviro + Air quality
+board and particular sensor.
+
+Its a first rough approach, only for demo.
+
+In this version we use Flask with it built in server to implement the
+REST-API.
+
+
+Note: the usage of the display on the enviro is only for easy checking while
+testing.
+
+"""
+
 from enviroplus import gas
+from bme280 import BME280
+from pms5003 import PMS5003, ReadTimeoutError
+import ST7735
+
+from datetime import datetime
 from flask import Flask
 from PIL import Image, ImageDraw, ImageFont
 from fonts.ttf import RobotoMedium as UserFont
 import logging
+from smbus import SMBus
 
 logging.basicConfig(
     format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
@@ -46,6 +65,7 @@ back_colour = (0, 170, 170)
 
 # TODO use helper functions
 
+
 # initial text
 line1 = datetime.now().strftime("%H%M%S")
 size_x, size_y = draw.textsize(line1, font)
@@ -66,25 +86,29 @@ draw.text((x, y), line2, font=font, fill=text_colour)
 disp.display(img)
 
 
-#--------
-# Retrieving gas
+# initilise bus for conenction to sensor
+bus = SMBus(1)
+# BME280 Weather data interface or temperature/pressure/humidity sensor
+bme280 = BME280(i2c_dev=bus)
+# PMS5003 particulate sensor
+pms5003 = PMS5003()
 
-# dont forget to enable adc
 
 gas.enable_adc()
 gas.set_adc_gain(4.096)
 
-
-
+# --------
+# Retrieving gas
 @rest_api_app.route('/gas')
 def get_gas_measurements():
 
-    line1 = datetime.now().strftime("%H%M%S")
+    now = datetime.utcnow()
+    line1 = now.strftime("%H%M%S")
     size_x, size_y = draw.textsize(line1, font)
 
     # text position
-    x = 0 
-    y = 0 
+    x = 0
+    y = 0
 
     # Draw background rectangle and write text.
     draw.rectangle((0, 0, 160, 80), back_colour)
@@ -99,12 +123,121 @@ def get_gas_measurements():
 
     # retrieve gas readings
     gas_readings = gas.read_all()
-    
-    return '{ "oxidised":' + str(gas_readings.oxidising) + \
-           ', "reduced":'  + str(gas_readings.reducing)  + \
+
+    return '{ "instant":' + now.isoformat() + \
+           ', "adc":' + str(gas_readings.adc) + \
            ', "nh3":' + str(gas_readings.nh3) + \
-           ', "adc":' + str(gas_readings.adc) + ' }'
- 
+           ', "oxidised":' + str(gas_readings.oxidising) + \
+           ', "reduced":' + str(gas_readings.reducing) + \
+           '}'
+
 
 # ---- retrieving pollution
+@rest_api_app.route('/pollution')
+def get_pollution_measurements():
+
+    now = datetime.utcnow()
+    line1 = now.strftime("%H%M%S")
+    size_x, size_y = draw.textsize(line1, font)
+
+    # text position
+    x = 0
+    y = 0
+
+    # Draw background rectangle and write text.
+    draw.rectangle((0, 0, 160, 80), back_colour)
+    draw.text((x, y), line1, font=font, fill=text_colour)
+
+    line2 = "pollution"
+    size_x, size_y = draw.textsize(line2, font)
+    y += size_y
+    draw.text((x, y), line2, font=font, fill=text_colour)
+
+    disp.display(img)
+
+    # retrieve pollution readings
+    values = {}
+    pm_values = pms5003.read()
+    try:
+        pm_values = pms5003.read()
+        # per m3
+        values["PM1_0"] = str(pm_values.pm_ug_per_m3(1.0))
+        values["PM2_5"] = str(pm_values.pm_ug_per_m3(2.5))
+        values["PM10"] = str(pm_values.pm_ug_per_m3(10))
+        values["PM1_0_atm"] = str(pm_values.pm_ug_per_m3(1.0, True))
+        values["PM2_5_atm"] = str(pm_values.pm_ug_per_m3(2.5, True))
+        values["PM10_atm"] = str(pm_values.pm_ug_per_m3(None, True))
+        # per liter
+        values["gt0_3um"] = str(pm_values.pm_per_1l_air(0.3))
+        values["gt0_5um"] = str(pm_values.pm_per_1l_air(0.5))
+        values["gt1_0um"] = str(pm_values.pm_per_1l_air(1.0))
+        values["gt2_5um"] = str(pm_values.pm_per_1l_air(2.5))
+        values["gt5_0um"] = str(pm_values.pm_per_1l_air(5))
+        values["gt10um"] = str(pm_values.pm_per_1l_air(10))
+    except ReadTimeoutError:
+        pms5003.reset()
+        pm_values = pms5003.read()
+        # per m3
+        values["PM1_0"] = str(pm_values.pm_ug_per_m3(1.0))
+        values["PM2_5"] = str(pm_values.pm_ug_per_m3(2.5))
+        values["PM10"] = str(pm_values.pm_ug_per_m3(10))
+        values["PM1_0_atm"] = str(pm_values.pm_ug_per_m3(1.0, True))
+        values["PM2_5_atm"] = str(pm_values.pm_ug_per_m3(2.5, True))
+        values["PM10_atm"] = str(pm_values.pm_ug_per_m3(None, True))
+        # per liter
+        values["gt0_3um"] = str(pm_values.pm_per_1l_air(0.3))
+        values["gt0_5um"] = str(pm_values.pm_per_1l_air(0.5))
+        values["gt1_0um"] = str(pm_values.pm_per_1l_air(1.0))
+        values["gt2_5um"] = str(pm_values.pm_per_1l_air(2.5))
+        values["gt5_0um"] = str(pm_values.pm_per_1l_air(5))
+        values["gt10um"] = str(pm_values.pm_per_1l_air(10))
+
+    return '{ "instant":' + now.isoformat() + \
+           ', "PM1_0":' + values["PM1_0"] + \
+           ', "PM2_5":' + values["PM2_5"] + \
+           ', "PM10":' + values["PM10"] + \
+           ', "PM1_0_atm":' + values["PM1_0_atm"] + \
+           ', “PM2_5_atm":' + values["PM2_5_atm"] + \
+           ', "PM10_atm":' + values["PM10_atm"] + \
+           ', "gt0_3um":' + values["gt0_3um"] + \
+           ', "gt0_5um":' + values["gt0_5um"] + \
+           ', "gt1_0um":' + values["gt1_0um"] + \
+           ', "gt2_5um":' + values["gt2_5um"] + \
+           ', "gt5_0um":' + values["gt5_0um"] + \
+           ', "gt10um":' + values["gt10um"] + \
+           '}'
+
+
+# ---- retrieving weatherdata
+@rest_api_app.route('/weather')
+def get_weather_measurements():
+    now = datetime.utcnow()
+    line1 = now.strftime("%H%M%S")
+    size_x, size_y = draw.textsize(line1, font)
+
+    # text position
+    x = 0
+    y = 0
+
+    # Draw background rectangle and write text.
+    draw.rectangle((0, 0, 160, 80), back_colour)
+    draw.text((x, y), line1, font=font, fill=text_colour)
+
+    line2 = "weather"
+    size_x, size_y = draw.textsize(line2, font)
+    y += size_y
+    draw.text((x, y), line2, font=font, fill=text_colour)
+
+    disp.display(img)
+
+    # retrieve weather readings
+    temperature = bme280.get_temperature()
+    pressure = bme280.get_pressure()
+    humidity = bme280.get_humidity()
+
+    return '{ "instant":' + now.isoformat() + \
+           ', "temperature":' + str(temperature) + \
+           ', "pressure":' + str(pressure) + \
+           ', "humidity":' + str(humidity) + \
+           '}'
 
